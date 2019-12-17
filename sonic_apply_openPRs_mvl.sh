4649984639acb00478f4664f5299d72ac66f5510
Antony Rheneus <arheneus@marvell.com>
2019-12-17 13:00:03 +0530

    sonic utilities PR 731 is closed and reopened as 769 to fix merge conflicts

--- sonic_apply_openPRs_mvl.sh
+++ sonic_apply_openPRs_mvl.sh
@@ -19,7 +19,7 @@ urlsai="https://patch-diff.githubusercontent.com/raw/opencomputeproject"
 declare -a PATCHES=(P1 P2 P3 P4 P5)
 declare -A P1=( [NAME]=sonic-buildimage [DIR]=. [PR]="3392 3644 3687 3734 3919" [URL]="$url" [PREREQ]="" )
 declare -A P2=( [NAME]=sonic-swss [DIR]=src/sonic-swss [PR]=1015 [URL]="$url" [PREREQ]="" )
-declare -A P3=( [NAME]=sonic-utilities [DIR]=src/sonic-utilities [PR]=731 [URL]="$url" [PREREQ]="" )
+declare -A P3=( [NAME]=sonic-utilities [DIR]=src/sonic-utilities [PR]=769 [URL]="$url" [PREREQ]="" )
 declare -A P4=( [NAME]=SAI [DIR]=src/sonic-sairedis/SAI [PR]="999" [URL]="$urlsai" [PREREQ]="" )
 declare -A P5=( [NAME]=sonic-linux-kernel [DIR]=src/sonic-linux-kernel [PR]=102 [URL]="$url" [PREREQ]="prereq_kernel" )
 
