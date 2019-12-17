b3ba33aac437ef6c5b64b343fbf6c8add060981b
Antony Rheneus <arheneus@marvell.com>
2019-12-17 11:35:24 +0530

    Added PR 3919 to address port config

--- sonic_apply_openPRs_mvl.sh
+++ sonic_apply_openPRs_mvl.sh
@@ -17,7 +17,7 @@ url="https://github.com/Azure"
 urlsai="https://patch-diff.githubusercontent.com/raw/opencomputeproject"
 
 declare -a PATCHES=(P1 P2 P3 P4 P5)
-declare -A P1=( [NAME]=sonic-buildimage [DIR]=. [PR]="3392 3644 3687 3734" [URL]="$url" [PREREQ]="" )
+declare -A P1=( [NAME]=sonic-buildimage [DIR]=. [PR]="3392 3644 3687 3734 3919" [URL]="$url" [PREREQ]="" )
 declare -A P2=( [NAME]=sonic-swss [DIR]=src/sonic-swss [PR]=1015 [URL]="$url" [PREREQ]="" )
 declare -A P3=( [NAME]=sonic-utilities [DIR]=src/sonic-utilities [PR]=731 [URL]="$url" [PREREQ]="" )
 declare -A P4=( [NAME]=SAI [DIR]=src/sonic-sairedis/SAI [PR]="999" [URL]="$urlsai" [PREREQ]="" )
